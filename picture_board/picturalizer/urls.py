1cd53cb5bd75026efc3f9ed3c775432faa9c7766
nanana <sazanami@gmail.com>
2025-01-10 22:07:31 +0900

    add thread page

--- picture_board/picturalizer/urls.py
+++ picture_board/picturalizer/urls.py
@@ -5,6 +5,6 @@ app_name = 'picturalizer'
 
 urlpatterns = [
     path('showall/', views.showall, name='showall'),
-    path('upload/', views.upload, name='upload'),
-    path('<int:image_id>/thread', views.thread, name='thread')
+    path('upload/', views.UploadView.as_view(), name='upload'),
+    path('<int:pk>/thread', views.ThreadView.as_view(), name='thread')
 ]
--- picture_board/picturalizer/views.py
+++ picture_board/picturalizer/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from django.views.generic import CreateView, ListView, DetailView
+from django.urls import reverse
+from django.http import Http404
+
 from .models import Image, Comment
 from .forms import ImageForm
 
@@ -15,13 +19,41 @@ def upload(request):
     context = {'form':form}
     return render(request, 'picturalizer/upload.html', context)
 
+class UploadView(CreateView):
+    template_name = 'picturalizer/upload.html'
+    form_class = ImageForm
+
+    def get_success_url(self):
+        return reverse('picturalizer:showall')
+
+
+
 def showall(request):
     images = Image.objects.all()
     context = {'images':images}
     return render(request, 'picturalizer/show_all_thread.html', context)
 
+class ShowAllView(ListView):
+    model = Image
+    template_name = 'picturalizer/show_all_thread.html'
+
+
 
 def thread(request, image_id):
     images = Image.objects.all()[0]
     context = {'images':images}
-    return render(request, 'picturalizer/thread.html', context)
+    return render(request, '', context)
+
+class ThreadView(DetailView):
+    model = Image
+    template_name = 'picturalizer/thread.html'
+
+    # def get_context_data(self, **kwargs):
+    #     print("*"*200)
+    #     for i in self.request:
+    #         print(i)
+    #     print("*"*200)
+    #     context = super().get_context_data(**kwargs)
+    #     comment = Comment.objects.filter(image=self.request.POST["pk"])
+    #     context['comments'] = comment  # コメントを取得
+    #     return context
